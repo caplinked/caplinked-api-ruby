5351e3e77a503ef029eefa75fde0a4a11fc27e30
Jazz Garcha <jaspreet@garcha.com>
2016-11-23 14:34:38 -0800

    finished files, add util methods for delete, put, post switch to rpsec, vcr, webmock remove expiring code

---
+++ spec/files_spec.rb
@@ -0,0 +1,76 @@
+require 'spec_helper'
+require 'dotenv'
+Dotenv.load
+
+describe Caplinked::Client, :vcr => true do
+  let(:key) { ENV["API_KEY"] }
+
+  it 'Upload a File' do
+    VCR.use_cassette("files/upload_file") do
+      client = Caplinked::Client.new api_key: key, api_scheme: 'https'
+      new_file = client.upload_file workspace_id: 5886, folder_id: 32277, file_name: 'pg_0317.pdf', file: File.read('/Users/jazz/Dropbox/TestData/CukeBook 2/index/pg_0317.pdf')
+
+      expect(new_file[:file_name]).to eq("pg_0317.pdf")
+    end
+  end
+
+  it 'Returns file info' do
+    VCR.use_cassette("files/file_info") do
+      client = Caplinked::Client.new api_key: key, api_scheme: 'https'
+      file_info = client.get_file_info id: 37639, workspace_id: 5886
+
+      expect(file_info[:id]).to eq(37639)
+    end
+  end
+
+  xit 'Gets file viewer' do
+    VCR.use_cassette("files/file_viewer") do
+      client = Caplinked::Client.new api_key: key, api_scheme: 'https'
+      file_viewer = client.get_file_viewer id: 37639, workspace_id: 5886, expiring_token: 'removed'
+
+      expect(file_info[:id]).to eq(37639)
+    end
+  end
+
+  it 'Deletes file' do
+    VCR.use_cassette("files/delete_file") do
+      client = Caplinked::Client.new api_key: key, api_scheme: 'https'
+      delete_file = client.delete_file workspace_id: 5886, id: 37639
+
+      expect(delete_file[:id]).to eq(37639)
+      expect(delete_file[:deleted]).to eq(true)
+    end
+  end
+
+  it 'Update file info' do
+    VCR.use_cassette("files/update_file_info") do
+      client = Caplinked::Client.new api_key: key, api_scheme: 'https'
+      update_file = client.update_file_info workspace_id: 5886, id: 45906, 'file[title]': 'api_update', 'file[index]': 1
+
+      expect(update_file[:title]).to eq('api_update.pdf')
+      expect(update_file[:index]).to eq(1)
+    end
+  end
+
+  xit 'Copy File:' do
+    VCR.use_cassette("files/copy_file") do
+      client = Caplinked::Client.new api_key: key, api_scheme: 'https'
+      copy_file = client.copy_file id: 45906, workspace_id: 5886, destination_folder_id: 32339
+
+      # needs copy/move fix
+      # binding.pry
+
+      expect(file_info[:id]).to eq(45907)
+    end
+  end
+
+  it 'Move File:' do
+    VCR.use_cassette("files/move_file") do
+      client = Caplinked::Client.new api_key: key, api_scheme: 'https'
+      move_file = client.move_file id: 45907, workspace_id: 5886, destination_folder_id: 36885
+
+      expect(move_file[:id]).to eq(45907)
+    end
+  end
+
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,38 @@
+require 'simplecov'
+require 'vcr'
+require 'pry'
+
+module SimpleCov::Configuration
+  def clean_filters
+    @filters = []
+  end
+end
+
+SimpleCov.configure do
+  clean_filters
+  load_adapter 'test_frameworks'
+end
+
+ENV["COVERAGE"] && SimpleCov.start do
+  add_filter "/.rvm/"
+end
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+
+require 'rspec'
+require 'caplinked-api-ruby'
+
+# Requires supporting files with custom matchers and macros, etc,
+# in ./support/ and its subdirectories.
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+
+RSpec.configure do |config|
+
+end
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/fixtures/cassettes'
+  c.hook_into :webmock
+  #hides API_KEY in the cassettes
+  c.filter_sensitive_data('<ENCODED X-Token>'){ ENV["API_KEY"] }
+end

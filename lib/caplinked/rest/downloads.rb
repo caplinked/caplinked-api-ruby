fe506fb7ecbe014cacc29befe0022d1c52084eca
Jazz Garcha <jaspreet@garcha.com>
2016-12-01 16:54:48 -0800

    add downloads methods and tests

--- lib/caplinked/rest/downloads.rb
+++ lib/caplinked/rest/downloads.rb
@@ -10,6 +10,30 @@ module Caplinked
         body = options.stringify_keys.slice('workspace_id', 'download')
         perform_post('/api/v1/downloads', params, body.to_json, { 'Content-Type' => 'application/json' })
       end
+
+      def single_file_download(options = {})
+        params = options.stringify_keys.slice('file_id', 'workspace_id')
+        file_id = params.delete('file_id')
+        perform_get('/api/v1/downloads/file/' + file_id.to_s, params)
+      end
+
+      def download_status(options = {})
+        params = options.stringify_keys.slice('workspace_id')
+        workspace_id = params.delete('workspace_id')
+        perform_get('/api/v1/downloads/status/' + workspace_id.to_s, nil)
+      end
+
+      def delete_download(options = {})
+        params = options.stringify_keys.slice('id', 'workspace_id')
+        id = params.delete('id')
+        perform_delete('/api/v1/downloads/' + id.to_s, params)
+      end
+
+      def get_zip(options = {})
+        params = options.stringify_keys.slice('id', 'workspace_id')
+        id = params.delete('id')
+        perform_get('/api/v1/downloads/' + id.to_s, params)
+      end
     end
   end
 end
--- spec/downloads_spec.rb
+++ spec/downloads_spec.rb
@@ -8,10 +8,49 @@ describe Caplinked::Client, :vcr => true do
 
       create_zip_file = client.create_zip_file workspace_id: 5886, download: { folder_ids: [32277,32339] }
 
-      #need update
       expect(create_zip_file).to eq({:id=>create_zip_file[:id], :user_id=>2,
           :file_ids=>[], :folder_ids=>[32277,32339], :status=>"building_directory",
           :created_at=>create_zip_file[:created_at]})
+    end
+  end
+
+  xit 'download single file' do
+    VCR.use_cassette("downloads/download_single_file") do
+
+      single_file_download = client.single_file_download file_id: 45907, workspace_id: 5886
+
+      #returns file
+      #HTTP::Error: Unknown MIME type:
+      expect(single_file_download).to eq()
+    end
+  end
+
+  it 'download single file' do
+    VCR.use_cassette("downloads/download_status") do
+
+      download_status = client.download_status workspace_id: 5886
+
+      expect(download_status.first).to eq({"id"=>download_status.first['id'], "user_id"=>2, "file_ids"=>[], "folder_ids"=>[32277, 32339], "status"=>"done", "created_at"=>download_status.first['created_at']})
+    end
+  end
+
+  it 'download single file' do
+    VCR.use_cassette("downloads/delete_download") do
+
+      delete_download = client.delete_download id: 713, workspace_id: 5886
+
+      expect(delete_download).to eq({:id=>delete_download[:id], :status=>200})
+    end
+  end
+
+  xit 'get zip' do
+    VCR.use_cassette("downloads/get_zip") do
+
+      get_zip = client.get_zip id: 714, workspace_id: 5886
+
+      #returns zip
+      #HTTP::Error: Unknown MIME type:
+      expect(get_zip).to eq()
     end
   end
 

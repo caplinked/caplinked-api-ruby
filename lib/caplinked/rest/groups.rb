83a1abd531e5d224df9e6a2da3f6532ba3b87966
Jazz Garcha <jazz@caplinked.com>
2016-12-05 13:33:40 -0800

    add group endpoint

--- lib/caplinked/rest/groups.rb
+++ lib/caplinked/rest/groups.rb
@@ -1,6 +1,84 @@
 module Caplinked
   module REST
     module Groups
+
+      def list_all_groups_in_workspace(options = {})
+        params = options.stringify_keys.slice('workspace_id')
+        perform_get('/api/v1/groups', params)
+      end
+
+      def create_group(options = {})
+        body = options.stringify_keys.slice('group')
+        perform_post('/api/v1/groups', nil, body.to_json, {'Content-Type' => 'application/json'} )
+      end
+
+      def update_group(options = {})
+        body = options.stringify_keys.slice('id', 'workspace_id', 'group')
+        id = body.delete('id')
+        perform_put('/api/v1/groups/' + id.to_s, nil, body.to_json, { 'Content-Type' => 'application/json' } )
+      end
+
+      def get_group_info(options = {})
+        params = options.stringify_keys.slice('id', 'workspace_id')
+        id = params.delete('id')
+        perform_get('/api/v1/groups/' + id.to_s, params)
+      end
+
+      def delete_group(options = {})
+        params = options.stringify_keys.slice('id', 'workspace_id')
+        id = params.delete('id')
+        perform_delete('/api/v1/groups/' + id.to_s, params)
+      end
+
+      def update_group_drm(options = {})
+        body = options.stringify_keys.slice('id', 'workspace_id', 'group')
+        id = body.delete('id')
+        perform_put('/api/v1/groups/' + id.to_s + '/drm', nil, body.to_json, { 'Content-Type' => 'application/json' } )
+      end
+
+      def disable_drm_expiration(options = {})
+        body = options.stringify_keys.slice('id', 'workspace_id')
+        id = body.delete('id')
+        perform_put('/api/v1/groups/' + id.to_s + '/disable_drm_expiration', nil, body.to_json, { 'Content-Type' => 'application/json' } )
+      end
+
+      def watermarking_for_group(options = {})
+        body = options.stringify_keys.slice('id', 'workspace_id', 'group')
+        id = body.delete('id')
+        perform_put('/api/v1/groups/' + id.to_s + '/watermarking', nil, body.to_json, { 'Content-Type' => 'application/json' } )
+      end
+
+      def enable_access_expiration(options = {})
+        body = options.stringify_keys.slice('id', 'workspace_id', 'group')
+        id = body.delete('id')
+        perform_put('/api/v1/groups/' + id.to_s + '/enable_expire_access', nil, body.to_json, { 'Content-Type' => 'application/json' } )
+      end
+
+      def disable_access_expiration(options = {})
+        body = options.stringify_keys.slice('id', 'workspace_id')
+        id = body.delete('id')
+        perform_put('/api/v1/groups/' + id.to_s + '/disable_expire_access', nil, body.to_json, { 'Content-Type' => 'application/json' } )
+      end
+
+      def add_group_member(options = {})
+        params = {}
+        body = options.stringify_keys.slice('id', 'user_id', 'workspace_id', 'send_email')
+        id = body.delete('id')
+        perform_post('/api/v1/groups/' + id.to_s + '/memberships', params, body.to_json, { 'Content-Type' => 'application/json' } )
+      end
+
+      def remove_group_member(options = {})
+        params = options.stringify_keys.slice('id', 'user_id', 'workspace_id')
+        id = params.delete('id')
+        perform_delete('/api/v1/groups/' + id.to_s + '/memberships', params )
+      end
+
+      def list_all_group_members(options = {})
+        params = options.stringify_keys.slice('id', 'workspace_id')
+        id = params.delete('id')
+        perform_get('/api/v1/groups/' + id.to_s + '/memberships', params )
+      end
+
     end
   end
 end

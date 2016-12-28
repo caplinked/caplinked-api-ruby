78284b94be37f0ec4afeb9c6cd648e6338de8e84
Jazz Garcha <jaspreet@garcha.com>
2016-12-28 15:08:50 -0800

    remove stub

--- spec/core/request.rb
+++ spec/core/request.rb
@@ -4,10 +4,6 @@ RSpec.describe "Request", :type => :request do
   client = Caplinked::Client.new api_key: "API_KEY", api_host: 'sandbox.caplinked.com', api_scheme: 'https'
 
   it "execute init" do
-    stub_request(:get, "https://sandbox.caplinked.com/api/v1/files/38187?workspace_id=5886").
-      with(headers: {'Connection'=>'close', 'Host'=>'sandbox.caplinked.com', 'User-Agent'=>'http.rb/2.1.0', 'X-Token'=> "API_KEY"}).
-      to_return(status: 200, body: fixture('activities_workspace.json'), headers: { content_type: 'application/json; charset=utf-8' })
-
     request = Caplinked::Request.new(client, :get, "/api/v1/activities/workspace/5886", {:params=>{}})
 
     expect(request.client.api_host).to eq("sandbox.caplinked.com")
